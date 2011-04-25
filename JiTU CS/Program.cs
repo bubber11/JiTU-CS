6e52274ee3789c0aabc2bab68f10104b1eb5caf2
Robert Novak <robert.novak@asu.edu>
2011-04-24 23:11:37 -0700

    Finished course entity, started work on question entity, and tested course entity.

--- JiTU CS/Program.cs
+++ JiTU CS/Program.cs
@@ -10,13 +10,17 @@ namespace JiTU_CS
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+
         [STAThread]
         static void Main()
         {
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new UI.GeneralUI());
-            Console.WriteLine("Hello World");
+
+            Entity.QuestionEntity temp = new Entity.QuestionEntity();
+
         }
     }
 }
