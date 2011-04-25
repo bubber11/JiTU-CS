6baac5bc780bdd1aa2d8e9cddb9abd910397252d
sthatch <tylerh@randkaz.com>
2011-04-25 10:54:20 -0700

    Added the functions "GetStudents" and "GetStudentsByCourse" to the UserEntity class

--- JiTU CS/Entity/UserEntity.cs
+++ JiTU CS/Entity/UserEntity.cs
@@ -11,6 +11,10 @@ namespace JiTU_CS.Entity
     {
         #region Methods
 
+        /// <summary>
+        /// Adds the user data to the database
+        /// </summary>
+        /// <param name="userData"></param>
         public void addUser (UserData userData)
         {
             try
@@ -34,6 +38,10 @@ namespace JiTU_CS.Entity
             }
         }
 
+        /// <summary>
+        /// Updated the user data in the database
+        /// </summary>
+        /// <param name="userData"></param>
         public void updateUser(UserData userData)
         {
             
@@ -60,6 +68,10 @@ namespace JiTU_CS.Entity
   
         }
 
+        /// <summary>
+        /// Deletes the given user from the database
+        /// </summary>
+        /// <param name="userData"></param>
         public void deleteUser(UserData userData)
         {
             try
@@ -76,6 +88,11 @@ namespace JiTU_CS.Entity
             }
         }
 
+        /// <summary>
+        /// Gets a UserData object based on the user's name
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         public UserData getUser(string userName)
         {
             UserData newUser = null;
@@ -113,6 +130,11 @@ namespace JiTU_CS.Entity
             return newUser;
         }
 
+        /// <summary>
+        /// Gets a UserData object based on the user's ID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
         public UserData getUser(int userID)
         {
             UserData newUser = null;
@@ -148,6 +170,97 @@ namespace JiTU_CS.Entity
             }
 
             return newUser;
+        }
+
+        /// <summary>
+        /// Gets a list of UserData objects representing all students currently in the database
+        /// </summary>
+        /// <returns></returns>
+        public List<UserData> GetStudents()
+        {
+
+            List<UserData> theList = new List<UserData>();
+
+            try
+            {
+                this.SQL = "SELECT * FROM `users WHERE u.`role_id` = 2;";
+                this.InitializeCommand();
+                this.OpenConnection();
+
+                this.DataReader = this.Command.ExecuteReader();
+
+                if (this.DataReader.HasRows)
+                {
+                    UserData newUser;
+
+                    while (this.DataReader.Read())
+                    {
+                        newUser = new UserData(this.DataReader.GetInt32("user_id"));
+                        newUser.FullName = this.DataReader.GetString("full_name");
+                        newUser.Password = this.DataReader.GetString("password");
+                        newUser.Role = (UserData.Roles)this.DataReader.GetInt32("role_id");
+                        newUser.UserName = this.DataReader.GetString("user_name");
+
+                        theList.Add(newUser);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+            return theList;
+        }
+
+        /// <summary>
+        /// Gets a list of UserData objects representing the students enrolled in a particular course
+        /// </summary>
+        /// <param name="CourseID"></param>
+        /// <returns></returns>
+        public List<UserData> GetStudentsByCourse(int CourseID)
+        {
+            List<UserData> theList = new List<UserData>();
+
+            try
+            {
+                this.SQL = "SELECT u.* FROM `users` u JOIN `rel_courses_users` uc ON u.`user_id` = uc.`user_id` " +
+                    "WHERE u.`role_id` = 2 AND uc.`course_id` = " + CourseID + ";";
+                this.InitializeCommand();
+                this.OpenConnection();
+
+                this.DataReader = this.Command.ExecuteReader();
+
+                if (this.DataReader.HasRows)
+                {
+                    UserData newUser;
+
+                    while (this.DataReader.Read())
+                    {
+                        newUser = new UserData(this.DataReader.GetInt32("user_id"));
+                        newUser.FullName = this.DataReader.GetString("full_name");
+                        newUser.Password = this.DataReader.GetString("password");
+                        newUser.Role = (UserData.Roles)this.DataReader.GetInt32("role_id");
+                        newUser.UserName = this.DataReader.GetString("user_name");
+
+                        theList.Add(newUser);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+            return theList;
         }
 
         #endregion
