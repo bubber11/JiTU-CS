a5e943505d15d530e08309f52fb7bfd13fc331f3
sthatch <tylerh@randkaz.com>
2011-04-29 22:55:43 -0700

    Tried to work on the student views

--- JiTU CS/UI/Screens/StudentScreen.cs
+++ JiTU CS/UI/Screens/StudentScreen.cs
@@ -24,7 +24,7 @@ namespace JiTU_CS.UI.Screens
 
         private void btnTakeQuiz_Click(object sender, EventArgs e)
         {
-
+			DisplayView(new Views.ClassesView(ViewTypes.Quizzes));
         }
 
         private void gotoQuizesView(object sender, EventArgs e)
